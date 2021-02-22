b86f67a7bdc842edfb14df4fd4dbca82cbc7c772
Alexande B <abobrikovich@gmail.com>
2021-02-22 21:03:39 +0300

    Add .rubocop.yml and fix issues

--- lib/mn2pdf.rb
+++ lib/mn2pdf.rb
@@ -1,47 +1,48 @@
-require 'open3'
-require 'rbconfig'
-require 'mn2pdf/version'
+require "open3"
+require "rbconfig"
+require "mn2pdf/version"
 
 module Mn2pdf
-  MN2PDF_JAR_PATH = File.join(File.dirname(__FILE__), '../bin/mn2pdf.jar')
-
-  def self.help
-    cmd = ['java', '-jar', MN2PDF_JAR_PATH].join(' ')
-    message, error_str, status = Open3.capture3(cmd)
-    message
-  end
-
-  def self.version
-    cmd = ['java', '-jar', MN2PDF_JAR_PATH, '-v'].join(' ')
-    message, error_str, status = Open3.capture3(cmd)
-    message.strip
-  end
+  MN2PDF_JAR_PATH = File.join(File.dirname(__FILE__), "../bin/mn2pdf.jar")
 
   def self.jvm_options
-    options = ['-Xss5m', '-Xmx1024m']
+    options = []
 
-    if RbConfig::CONFIG['host_os'].match?(/darwin|mac os/)
-      options << '-Dapple.awt.UIElement=true'
+    if RbConfig::CONFIG["host_os"].match?(/darwin|mac os/)
+      options << "-Dapple.awt.UIElement=true"
     end
 
     options
   end
 
+  def self.help
+    cmd = ["java", *jvm_options, "-jar", MN2PDF_JAR_PATH].join(" ")
+    message, = Open3.capture3(cmd)
+    message
+  end
+
+  def self.version
+    cmd = ["java", *jvm_options, "-jar", MN2PDF_JAR_PATH, "-v"].join(" ")
+    message, = Open3.capture3(cmd)
+    message.strip
+  end
+
   def self.convert(url_path, output_path, xsl_stylesheet, options = "")
     return if url_path.nil? || output_path.nil? || xsl_stylesheet.nil?
 
-    puts MN2PDF_JAR_PATH
-    cmd = ['java', *jvm_options, '-jar', MN2PDF_JAR_PATH, '--xml-file',
-           url_path, '--xsl-file', xsl_stylesheet, '--pdf-file',
-           output_path, options].join(' ')
+    cmd = ["java", "-Xss5m", "-Xmx1024m", *jvm_options,
+           "-jar", MN2PDF_JAR_PATH, "--xml-file", url_path,
+           "--xsl-file", xsl_stylesheet, "--pdf-file", output_path, options].join(" ")
 
     puts cmd
     stdout_str, error_str, status = Open3.capture3(cmd)
 
-    unless status.success?
-      # Strip default mn2pdf message
-      stdout_str = stdout_str.gsub('Preparing...', '').strip
-      raise [stdout_str, error_str].join(' ').strip
-    end
+    raise prepare_error_msg(stdout_str, error_str) unless status.success?
+  end
+
+  def self.prepare_error_msg(stdout_str, error_str)
+    # Strip default mn2pdf message
+    stdout_str = stdout_str.gsub("Preparing...", "").strip
+    [stdout_str, error_str].join(" ").strip
   end
 end
--- spec/mn2pdf_spec.rb
+++ spec/mn2pdf_spec.rb
@@ -1,33 +1,39 @@
-require 'tmpdir'
+require "tmpdir"
 
 RSpec.describe Mn2pdf do
-
-=begin
-  There is no reason why the gem version must equal the JAR version. Rescinded 
-  it 'matches the version number of JAR' do
+  it "matches the version number of JAR" do
     expect(Mn2pdf::VERSION).to eq(Mn2pdf.version)
   end
-=end
 
-  it 'converts XML to PDF' do
+  it "help not empty" do
+    expect(Mn2pdf.help).not_to be_empty
+  end
 
+  it "converts XML to PDF" do
     Dir.mktmpdir do |dir|
-      pdf_path = File.join(dir, 'G.191.pdf')
+      pdf_path = File.join(dir, "G.191.pdf")
 
       Mn2pdf.convert(sample_xml, pdf_path, sample_xsl)
       expect(File.exist?(pdf_path)).to be true
     end
+  end
 
+  it "raise an error on converting not existing XML" do
+    pdf_path = "missing.pdf"
+    xml_path = "missing.xml"
+    expect do
+      Mn2pdf.convert(xml_path, pdf_path, sample_xsl)
+    end.to raise_error(/XML file '#{xml_path}' not found!/)
+    expect(File.exist?(pdf_path)).to be false
   end
 
   let(:sample_xsl) do
-    Pathname.new(File.dirname(__dir__)).
-    join('spec', 'fixtures', 'itu.recommendation.xsl').to_s
+    Pathname.new(File.dirname(__dir__))
+      .join("spec", "fixtures", "itu.recommendation.xsl").to_s
   end
 
   let(:sample_xml) do
-    Pathname.new(File.dirname(__dir__)).
-    join('spec', 'fixtures', 'G.191.xml').to_s
+    Pathname.new(File.dirname(__dir__))
+      .join("spec", "fixtures", "G.191.xml").to_s
   end
-
 end
