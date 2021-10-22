2783d731fd013b69b8222742869de673cb8f721f
Alexande B <abobrikovich@gmail.com>
2021-10-22 20:11:32 +0200

    move quotes from isodoc gem to mn2pdf

--- lib/mn2pdf.rb
+++ lib/mn2pdf.rb
@@ -4,7 +4,7 @@ require "mn2pdf/version"
 
 module Mn2pdf
   MN2PDF_JAR_PATH = File.join(File.dirname(__FILE__), "../bin/mn2pdf.jar")
-  DEFAULT_JAVA_OPTS = ["-Xss5m", "-Xmx2048m"]
+  DEFAULT_JAVA_OPTS = ["-Xss5m", "-Xmx2048m"].freeze
 
   def self.jvm_options
     options = DEFAULT_JAVA_OPTS.dup
@@ -19,7 +19,7 @@ module Mn2pdf
   def self.help
     cmd = ["java", *jvm_options, "-jar", MN2PDF_JAR_PATH].join(" ")
     # help message goes to STDERR (from mn2pdf v1.36)
-    blank_message, help_message, = Open3.capture3(cmd)
+    _, help_message, = Open3.capture3(cmd)
     help_message
   end
 
@@ -34,9 +34,9 @@ module Mn2pdf
 
     cmd = ["java", *jvm_options,
            "-jar", MN2PDF_JAR_PATH,
-           "--xml-file", url_path,
-           "--xsl-file", xsl_stylesheet,
-           "--pdf-file", output_path, options].join(" ")
+           "--xml-file", quote(url_path),
+           "--xsl-file", quote(xsl_stylesheet),
+           "--pdf-file", quote(output_path), options].join(" ")
 
     puts cmd
     stdout_str, error_str, status = Open3.capture3(cmd)
@@ -48,5 +48,11 @@ module Mn2pdf
     # Strip default mn2pdf message
     stdout_str = stdout_str.gsub("Preparing...", "").strip
     ["[mn2pdf] Fatal:", stdout_str, error_str].join(" ").strip
+  end
+
+  def self.quote(str)
+    return str if /^'.*'$/.match(str) || /^".*"$/.match(str)
+
+    %("#{str}")
   end
 end
