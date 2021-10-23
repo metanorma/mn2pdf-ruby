f17451d418a1e8a0c746ffbd8cecadb2e8cacf07
Alexande B <abobrikovich@gmail.com>
2021-10-23 20:48:54 +0200

    metanorma/metanorma#215 move font manifest file generation from metanorma gem

--- spec/mn2pdf_spec.rb
+++ spec/mn2pdf_spec.rb
@@ -28,6 +28,26 @@ RSpec.describe Mn2pdf do
     expect(File.exist?(pdf_path)).to be false
   end
 
+  it "test font manifest hash options" do
+    Dir.mktmpdir do |dir|
+      pdf_path = File.join(dir, "G.191.pdf")
+      Mn2pdf.convert(sample_xml, pdf_path, sample_xsl,
+                     font_manifest: font_manifest)
+      expect(File.exist?(pdf_path)).to be true
+    end
+  end
+
+  it "test font manifest str options" do
+    Dir.mktmpdir do |dir|
+      pdf_path = File.join(dir, "G.191.pdf")
+      font_manifest_file = File.join(dir, "fm.yaml")
+      File.write(font_manifest_file, font_manifest.to_yaml)
+      Mn2pdf.convert(sample_xml, pdf_path, sample_xsl,
+                     "--font-manifest #{font_manifest_file}")
+      expect(File.exist?(pdf_path)).to be true
+    end
+  end
+
   let(:sample_xsl) do
     Pathname.new(File.dirname(__dir__))
       .join("spec", "fixtures", "itu.recommendation.xsl").to_s
@@ -36,5 +56,35 @@ RSpec.describe Mn2pdf do
   let(:sample_xml) do
     Pathname.new(File.dirname(__dir__))
       .join("spec", "fixtures", "G.191.xml").to_s
+  end
+
+  let(:font_manifest) do
+    fonts = File.join(ENV["HOME"], ".fontist", "fonts")
+    {
+      "Cambria Math" => {
+        "Regular" => {
+          "full_name" => "Cambria Math",
+          "paths" => ["#{fonts}/CAMBRIA.ttc"],
+        },
+      },
+      "Times New Roman" => {
+        "Bold Italic" => {
+          "full_name" => "Times New Roman Bold Italic",
+          "paths" => ["#{fonts}/TimesBI.ttf"],
+        },
+        "Italic" => {
+          "full_name" => "Times New Roman Italic",
+          "paths" => ["#{fonts}/TimesI.ttf"],
+        },
+        "Regular" => {
+          "full_name" => "Times New Roman",
+          "paths" => ["#{fonts}/Times.ttf"],
+        },
+        "Bold" => {
+          "full_name" => "Times New Roman Bold",
+          "paths" => ["#{fonts}/TimesBd.ttf"],
+        },
+      },
+    }
   end
 end
