f235276761b916ce1f0cb4c711bd50f9ca2c35ab
Alexande B <abobrikovich@gmail.com>
2024-08-06 01:46:47 +0200

    feat: handle JAVA_OPTS #39

--- lib/mn2pdf.rb
+++ lib/mn2pdf.rb
@@ -3,78 +3,61 @@ require "rbconfig"
 require "tempfile"
 require "yaml"
 
+require "mn2pdf/jvm"
+
 module Mn2pdf
   MN2PDF_JAR_PATH = File.join(File.dirname(__FILE__), "../bin/mn2pdf.jar")
-  DEFAULT_JAVA_OPTS = %w[-Xss10m -Xmx3g -Djava.awt.headless=true].freeze
   FONTS_MANIFEST = :font_manifest
 
-  def self.jvm_options
-    options = DEFAULT_JAVA_OPTS.dup
-
-    if RbConfig::CONFIG["host_os"].match?(/darwin|mac os/)
-      options << "-Dapple.awt.UIElement=true"
-    end
-
-    options << "-Duser.home=#{Dir.home}"
-
-    options
-  end
-
   def self.help
-    cmd = ["java", *jvm_options, "-jar", MN2PDF_JAR_PATH].join(" ")
     # help message goes to STDERR (from mn2pdf v1.36)
-    _, help_message, = Open3.capture3(cmd)
+    _, help_message, = Jvm.jar(MN2PDF_JAR_PATH)
     help_message
   end
 
   def self.version
-    cmd = ["java", *jvm_options, "-jar", MN2PDF_JAR_PATH, "-v"].join(" ")
-    message, = Open3.capture3(cmd)
+    message, = Jvm.jar(MN2PDF_JAR_PATH, %w[-v])
     message.strip
   end
 
   def self.convert(url_path, output_path, xsl_stylesheet, options = {})
-    cmd = build_cmd(url_path, output_path, xsl_stylesheet)
+    args = build_args(url_path, output_path, xsl_stylesheet)
 
-    return unless cmd
+    return unless args
 
     case options
     when String
-      mn2pdf(cmd + [options])
+      mn2pdf(args + [options])
     when Hash
-      mn2pdf_hash(cmd, options)
+      mn2pdf_hash(args, options)
     else
       warn "Unsupported options type #{options.class}"
     end
   end
 
-  def self.build_cmd(url, output, xslt)
+  def self.build_args(url, output, xslt)
     return if url.nil? || output.nil? || xslt.nil?
 
-    ["java", *jvm_options,
-     "-jar", MN2PDF_JAR_PATH,
-     "--xml-file", quote(url),
+    ["--xml-file", quote(url),
      "--xsl-file", quote(xslt),
      "--pdf-file", quote(output)]
   end
 
-  def self.mn2pdf_hash(cmd, options)
+  def self.mn2pdf_hash(args, options)
     manifest = options.delete(FONTS_MANIFEST)
-    options_to_cmd(options, cmd)
+    options_to_cmd(options, args)
     if manifest
       dump_fontist_manifest_locations(manifest) do |manifest_path|
-        cmd << "--font-manifest" << quote(manifest_path)
-        mn2pdf(cmd)
+        args << "--font-manifest" << quote(manifest_path)
+        mn2pdf(args)
       end
     else
-      mn2pdf(cmd)
+      mn2pdf(args)
     end
   end
 
-  def self.mn2pdf(cmd)
-    cmd = cmd.join(" ")
-    puts cmd
-    stdout, stderr, status = Open3.capture3(cmd)
+  def self.mn2pdf(args)
+    stdout, stderr, status = Jvm.jar(MN2PDF_JAR_PATH, args)
 
     unless status.success?
       puts_error_log(stdout, stderr)
---
+++ lib/mn2pdf/jvm.rb
@@ -0,0 +1,54 @@
+require "open3"
+require "rbconfig"
+
+module Jvm
+  DEFAULT_JAVA_OPTS = %w[-Xss10m -Xmx3g -Djava.awt.headless=true].freeze
+
+  def self.to_bytes(value)
+    case value[-1].downcase
+    when "k"
+      value.to_i * 1024
+    when "m"
+      value.to_i * 1024 * 1024
+    when "g"
+      value.to_i * 1024 * 1024 * 1024
+    else
+      value.to_i
+    end
+  end
+
+  def self.ensure_java_opts(opts, key, min_value_bytes)
+    flag, idx = opts.each_with_index.find { |opt, _| opt.start_with?(key) }
+    if flag
+      flag_value_bytes = to_bytes(flag[key.length..-1])
+
+      if flag_value_bytes < min_value_bytes
+        min_value_str = "#{key}#{min_value_bytes / (1024 * 1024)}m"
+        opts[idx] = min_value_str
+      end
+    else
+      opts << "#{key}#{min_value_bytes / (1024 * 1024)}m"
+    end
+  end
+
+  def self.options
+    result = ENV["JAVA_OPTS"]&.split || DEFAULT_JAVA_OPTS.dup
+
+    ensure_java_opts(result, "-Xss", to_bytes("10m"))
+    ensure_java_opts(result, "-Xmx", to_bytes("3g"))
+
+    if RbConfig::CONFIG["host_os"].match?(/darwin|mac os/)
+      result << "-Dapple.awt.UIElement=true"
+    end
+
+    result << "-Duser.home=#{Dir.home}"
+
+    result
+  end
+
+  def self.jar(jar, args = [])
+    cmd = ["java", *options, "-jar", jar, *args].join(" ")
+    stdout, stderr, status = Open3.capture3(cmd)
+    [stdout, stderr, status]
+  end
+end
